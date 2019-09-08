4a042d353ce7a31efaacdadc025492bc3afe4d3c
beimer <beimercampos.123@gmail.com>
2019-09-08 10:38:33 -0500

    creacion y edicion de la primera app

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
 
+#
+# from django.shortcuts import render
+from django.http import HttpResponse
+
+#utilities
+from datetime import datetime
+
+# TEMP: creation of posts array
+posts=[
+    {
+        'name':'Salud',
+        'user':'Beimer campos',
+        'timestamp':datetime.now().strftime('%b %dth,%Y - %H:%M hrs'),
+        'picture':'https://picsum.photos/200/200/?image=1036',
+
+    },
+    {
+        'name':'Riqueza',
+        'user':'Beimer campos',
+        'timestamp':datetime.now().strftime('%b %dth,%Y - %H:%M hrs'),
+        'picture':'https://picsum.photos/200/200/?image=903',
+
+    },
+    {
+        'name':'Felicidad',
+        'user':'Beimer campos',
+        'timestamp':datetime.now().strftime('%b %dth,%Y - %H:%M hrs'),
+        'picture':'https://picsum.photos/200/200/?image=1076',
+
+    }
+]
+def list_posts(request):
+    content=[]
+    for post in posts:
+        content.append("""
+            <p><strong>{name}</strong></p>
+            <p><small>{user} - <i>{timestamp}</i></small></p>
+            <figure><img src="{picture}" alt="no image" /></figure>
+        """.format(**post))
+    return HttpResponse('<br />'.join(content))
 # Create your views here.
