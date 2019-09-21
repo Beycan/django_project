1ad9d6fca240897f360eb5fd88883333f5242b76
beimer <beimercampos.123@gmail.com>
2019-09-20 21:01:37 -0500

    esto es otra forma de mostrar lo mismo

--- users/admin.py
+++ users/admin.py
@@ -4,4 +4,6 @@ from django.contrib import admin
 from users.models import Profile
 
 # Register your models here.
-admin.site.register(Profile)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    pass
